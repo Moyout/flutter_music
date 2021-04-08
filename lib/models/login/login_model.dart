6ac9fdc66cf6ca25fc288fa180b0e5bc17d10388
DJT <1067600623@qq.com>
2021-04-08 19:59:59 +0800

    login/sign

---
+++ lib/models/login/login_model.dart
@@ -0,0 +1,44 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_music/util/tools.dart';
+
+class LoginRequest {
+  static Future<LoginModel> getLogin({
+    String? userName,
+    String? passWord,
+    Map<String, dynamic>? headers,
+  }) async {
+    Map<String, dynamic> map = {"userName": userName ?? "", "passWord": passWord ?? ""};
+    var response = await BaseRequest().toPost(
+      "http://api.moyou.website/login",
+      data: FormData.fromMap(map),
+      options: Options(headers: headers),
+    );
+    LoginModel lModel = LoginModel.fromJson(response);
+    return lModel;
+  }
+}
+
+class LoginModel {
+  String? code;
+  String? message;
+  String? token;
+  String? userName;
+
+  LoginModel({this.code, this.message, this.token, this.userName});
+
+  LoginModel.fromJson(Map<String, dynamic> json) {
+    code = json['code'].toString();
+    message = json['message'].toString();
+    token = json['token_'];
+    userName = json['userName'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['code'] = this.code;
+    data['message'] = this.message.toString();
+    data['token_'] = this.token;
+    data['userName'] = this.userName;
+    return data;
+  }
+}
